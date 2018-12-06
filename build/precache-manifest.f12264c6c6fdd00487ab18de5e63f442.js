0515822be34b60af479dc4917d9efc7c2a15cb41
Ammercado <39321438+Ammercado@users.noreply.github.com>
2018-12-05 22:48:18 -0300

    Add files via upload

---
+++ build/precache-manifest.f12264c6c6fdd00487ab18de5e63f442.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "022599b66b7503730245aa564285b0e4",
+    "url": "/static/media/intercambio.022599b6.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cac041da6e1e93afead4",
+    "url": "/static/js/main.cac041da.chunk.js"
+  },
+  {
+    "revision": "6bfaca860c223c427319",
+    "url": "/static/js/1.6bfaca86.chunk.js"
+  },
+  {
+    "revision": "cac041da6e1e93afead4",
+    "url": "/static/css/main.0af4f379.chunk.css"
+  },
+  {
+    "revision": "6bfaca860c223c427319",
+    "url": "/static/css/1.727b66fe.chunk.css"
+  },
+  {
+    "revision": "cff9cec56edd547839ea1d50b67a4645",
+    "url": "/index.html"
+  }
+];
